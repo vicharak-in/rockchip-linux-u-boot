6378dd6b24acca2b4e58cd017e2dd6f8bdd5a1c5
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-04-10 15:26:25 +0530

    uboot: Add initial support for building uboot for axon and vaaman
    Change-Id: Ib315bdc8884bae147f2a78cdb85456d8828e9a62
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

---
+++ build-uboot.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Build U-Boot scipts for rk3399 and rk3588
+
+DEVICE=""
+DATE=$(date +%Y%m%d)
+
+# rk3399 and rk3588
+if [ "$1" = "rk3399" ]; then
+	DEVICE="vaaman"
+	./make.sh rk3399
+elif [ "$1" = "rk3588" ]; then
+	DEVICE="axon"
+	echo "Building U-Boot for RK3588"
+	./make.sh rk3588
+elif [ "$1" = "clean" ]; then
+	make clean -j$(nproc --all) && make distclean -j$(nproc --all)
+	exit 0
+else
+	echo "Usage: ./build-uboot.sh rk3399|rk3588"
+	exit 1
+fi
+
+# Build U-Boot
+./make.sh
+
+# Create output directory if it doesn't exist
+if [ "$1" = "rk3399" ]; then
+	if [ ! -d vaaman ]; then
+		mkdir -p $DEVICE
+	fi
+elif [ "$1" = "rk3588" ]; then
+	if [ ! -d axon ]; then
+		mkdir -p $DEVICE
+	fi
+else
+	echo "Usage: ./build-uboot.sh rk3399|rk3588"
+	exit 1
+fi
+
+cp -v uboot.img $DEVICE/uboot-"${DATE}".img
+cp -v idblock.bin $DEVICE/idblock-"${DATE}".bin
+if [ "$1" = "rk3399" ]; then
+	cp -v trust.img $DEVICE/trust-"${DATE}".img
+fi
