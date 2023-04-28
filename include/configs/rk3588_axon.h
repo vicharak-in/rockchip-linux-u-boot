4506d2b143f55ec65446d8411ebf4f3fe21303b8
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-04-28 13:00:48 +0530

    board: Add support for Vicharak Axon Board
    Change-Id: I7e2a478d4482d4e2e6759ee20d26983545c000be
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

---
+++ include/configs/rk3588_axon.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2023 Vicharak India
+ */
+
+#ifndef __RK3588_AXON_H
+#define __RK3588_AXON_H
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
+
+#include <configs/rk3588_common.h>
+
+#endif /* __RK3588_AXON_H */
