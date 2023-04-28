7c6669950e60bf3cd5dca1c7091f3129b79fc08c
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-04-28 15:04:44 +0530

    board: Add support for Vicharak Vaaman Board
    Change-Id: I7d17b2f4e51cd328319b2da3cab8f51f43b1f1b9
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

---
+++ include/configs/rk3399_vaaman.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2023 Vicharak India
+ */
+
+#ifndef __RK3399_VAAMAN_H
+#define __RK3399_VAAMAN_H
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdin=serial,usbkbd\0" \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
+
+#include <configs/rk3399_common.h>
+
+#endif /* __RK3399_VAAMAN_H */
