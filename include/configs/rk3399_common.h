b02da17bb6d69f4bcea76cff58ba6175ee8c453e
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-06-22 11:24:33 +0530

    include: rk(3399/3588): Define fdtfile for devicetreedir

--- include/configs/rk3399_common.h
+++ include/configs/rk3399_common.h
@@ -64,6 +64,7 @@
 #include <config_distro_bootcmd.h>
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	ENV_MEM_LAYOUT_SETTINGS \
+	"fdtfile=" FDTFILE \
 	"partitions=" PARTS_DEFAULT \
 	ROCKCHIP_DEVICE_SETTINGS \
 	RKIMG_DET_BOOTDEV \
