1cca7b558c745980a05110aba5782afa5844d820
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-09-23 12:03:03 +0530

    include: configs: vaaman: Enable eMMC SDMA support
    Also fix statements layout while at it.
    
    Change-Id: I068db12e9500956ae32f8bea1fdd22814644817d
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- include/configs/rk3399_vaaman.h
+++ include/configs/rk3399_vaaman.h
@@ -6,14 +6,18 @@
 #ifndef __RK3399_VAAMAN_H
 #define __RK3399_VAAMAN_H
 
+#include <configs/rk3399_common.h>
+
 #define ROCKCHIP_DEVICE_SETTINGS \
 		"stdin=serial,usbkbd\0" \
 		"stdout=serial,vidconsole\0" \
 		"stderr=serial,vidconsole\0"
 
-#define CONFIG_SYS_MMC_ENV_DEV		1	/* eMMC */
-#define CONFIG_SYS_MMC_MAX_BLK_COUNT	32768
+/* Enable eMMC SDMA support */
+#define CONFIG_MMC_SDHCI_SDMA
 
-#include <configs/rk3399_common.h>
+/* Set eMMC as the default boot device */
+#define CONFIG_SYS_MMC_ENV_DEV 1
+#define CONFIG_SYS_MMC_MAX_BLK_COUNT	32768
 
 #endif /* __RK3399_VAAMAN_H */
