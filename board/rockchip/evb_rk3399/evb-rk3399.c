e0ee2f2eec115564026529104b2f5c48c8f24426
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-11-02 11:59:41 +0530

    ARM: dts: Add support for Vicharak Vaaman board
    Change-Id: If79ffe92f6b6f12e0c50567a343f79c6a1c99b42
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- board/rockchip/evb_rk3399/evb-rk3399.c
+++ board/rockchip/evb_rk3399/evb-rk3399.c
@@ -8,7 +8,32 @@
 #include <init.h>
 #include <log.h>
 #include <asm/arch-rockchip/periph.h>
+#include <asm/arch-rockchip/cru.h>
+#include <asm/arch-rockchip/gpio.h>
+#include <asm/arch-rockchip/grf_rk3399.h>
+#include <asm/io.h>
+#include <linux/kernel.h>
 #include <power/regulator.h>
+#include <adc.h>
+#include <spl_gpio.h>
+
+#define KEY_DOWN_MIN_VAL        0
+#define KEY_DOWN_MAX_VAL        30
+
+int rockchip_dnl_key_pressed(void)
+{
+	unsigned int key_val;
+
+	if (adc_channel_single_shot("saradc", 1, &key_val)) {
+		printf("%s read recovery key failed\n", __func__);
+		return false;
+	}
+
+	if (key_val >= KEY_DOWN_MIN_VAL && key_val <= KEY_DOWN_MAX_VAL)
+		return true;
+	else
+		return false;
+}
 
 #ifndef CONFIG_SPL_BUILD
 int board_early_init_f(void)
