845b8967c1f6795c0b41e7c23ad6e7632996f80e
avi951 <avi951@avi951.avi951>
2024-11-26 12:50:05 +0530

    Axon : U-boot Support For Android 12
    	It gives OPTEE support in order to boot in Android
    
    Signed-off-by: avi951 <avi951@avi951.avi951>

--- arch/arm/mach-rockchip/make_fit_atf.sh
+++ arch/arm/mach-rockchip/make_fit_atf.sh
@@ -10,7 +10,9 @@ source ./${srctree}/arch/arm/mach-rockchip/fit_nodes.sh
 gen_header
 gen_uboot_node
 gen_bl31_node
-#gen_bl32_node
+if grep -q '^CONFIG_ANDROID_BOOTLOADER=y' .config ; then
+gen_bl32_node
+fi
 gen_mcu_node
 gen_loadable_node
 gen_kfdt_node
